2ba7dec89deab7d24782a6ae6d39450825f6e905
snu1v3r <snu1v3r@github.com>
2024-09-13 09:58:57 +0200

    Added using Home-Manager within Flakes

--- flake.nix
+++ flake.nix
@@ -4,19 +4,31 @@
 	
 	inputs = {
 	
-		nixpgks.url= "nixpkgs/nixos-24.05";
+
+		nixpkgs.url= "nixpkgs/nixos-24.05";
+		home-manager.url = "github:nix-community/home-manager/release-24.05";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 	} ;
 
-	outputs = { self, nixpkgs, ...}:
+	outputs = { self, nixpkgs, home-manager, ...}:
 		let
+			system = "x86_64-linux";
 			lib = nixpkgs.lib;
+			pkgs = nixpkgs.legacyPackages.${system};
 		in {
 		nixosConfigurations = {
 			nixos = lib.nixosSystem {
-				system = "x86_64-linux";
+				inherit system;
 				modules = [ ./configuration.nix ];
 
+			};
+		};
+
+		homeConfigurations = {
+			user = home-manager.lib.homeManagerConfiguration {
+				inherit pkgs;
+				modules = [ ./home.nix ];
 			};
 		};
 	} ;
