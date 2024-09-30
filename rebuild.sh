b7d9841171e508bb09e0654d5a20be33db183728
snu1v3r <snu1v3r@github.com>
2024-09-30 21:56:17 +0200

    modifies generation title to include nixos version and git status

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,8 @@ then
 else
 	SCRIPT_DIR=~/nixos-config
 fi
-today=`date +%Y%m%d-%H%M`
+hms=`date +%Y%m%d-%H%M`
 branch=`(git branch 2>/dev/null | sed -n '/^\* / { s|^\* ||; p; }')`
 revision=`(git rev-parse HEAD)`
-sudo NIXOS_LABEL="$today.$branch-${revision:0:7}" nixos-rebuild switch --impure --flake $SCRIPT_DIR#system
+nixversion=`nixos-version | sed 's/\(.*\) .*/\1/'`
+sudo NIXOS_LABEL="$nixversion-$hms.$branch-${revision:0:7}" nixos-rebuild switch --impure --flake $SCRIPT_DIR#system
