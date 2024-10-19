6f7a2a4c8b23491d383ce2a97222a0e799c98d72
snu1v3r <snu1v3r@github.com>
2024-10-19 19:46:46 +0200

    adds ida-free to reversing profile

---
+++ profiles/reversing/home.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, userSettings, ... }:
+
+{
+  
+  # The base configuration for the profile is taken from the `base/home.nix` file.
+  # This file contains all modification that are needed to create a normal daily driver.
+
+  imports = [
+    ../base/home.nix  # This creates the setup that is always needed
+    ../../user/apps/flameshot/flameshot.nix
+    ../../user/apps/plasma/plasma.nix
+    ../../user/apps/codium/codium.nix
+    ( ./. + "/../../user/apps" + ("/" + userSettings.browser + "/" + userSettings.browser) + ".nix") # This selects the terminal emulator
+	];
+
+
+  home.packages = with pkgs; [
+    ida-free
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  
+
+  # This can be used to manage environment variables
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+
+}
+
