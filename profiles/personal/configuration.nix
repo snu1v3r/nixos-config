1fe8a9e0f8133036a447f945d6fe15255cd68323
snu1v3r <snu1v3r@github.com>
2024-10-20 11:51:56 +0200

    adds vmware host to the personal profile

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -5,4 +5,10 @@
   [
   ../base/configuration.nix
   ];
+
+# This enables the machine to act as VMWare host. This is only needed for the personal profile, because This
+# is the only machine that will act as a host. The other machines will only be used for guests
+
+  virtualisation.vmware.host.enable = true;
+
 }
