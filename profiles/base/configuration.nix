309e6bb8d74c62fcedd854ec6da0357fdfb79082
snu1v3r <snu1v3r@github.com>
2024-10-20 12:41:24 +0200

    removed kde bloat

--- profiles/base/configuration.nix
+++ profiles/base/configuration.nix
@@ -64,6 +64,13 @@
   services.displayManager.sddm.enable = true;
 
   services.desktopManager.plasma6.enable = true;
+  
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    elisa
+    kate
+    khelpcenter
+  ];
 
   
   vm.guest-services.enable = systemSettings.vm-guest;
