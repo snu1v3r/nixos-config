c605cf46150dc7dce164468a863cc02eb5520d53
snu1v3r <snu1v3r@github.com>
2024-11-25 21:59:08 +0100

    changes lazygit settings
    adds various changes to the lazygit configuration:
    - sets neovim as the default editor
    - adds catppucchin mocha as default theme

--- user/shell/cli-collection.nix
+++ user/shell/cli-collection.nix
@@ -15,6 +15,23 @@ imports = [
     };
     lazygit = {
       enable = true;
+      settings = {
+        gui.theme = {
+          activeBorderColor = [ "#89b4fa" "bold" ];
+          inactiveBorderColor = [ "#a6adc8" ];
+          optionTextColor = [ "#89b4fa" ];
+          selectedLineBgColor = [ "#313244" ];
+          cherryPikcedCommitBgColor = [ "#45475a" ];
+          cherryPikcedCommitFgColor = [ "#89b4fa" ];
+          unstagedChangesColor = [ "#f38ba8" ];
+          defaultFgColor = [ "#cdd6f4" ];
+          searchingActiveBorderColor = [ "#f9e2af" ];
+        };
+        os = {
+          edit = "/etc/profiles/per-user/user/bin/nvim {{filename}}";
+          editAtLine = "/etc/profiles/per-user/user/bin/nvim {{filename}} +{{line}}";
+        };
+      };
     };
 	};
 
