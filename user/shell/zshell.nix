f32299ea0f8d8d6264c6339862984287e851f468
snu1v3r <snu1v3r@github.com>
2024-11-30 08:44:30 +0100

    moved loading of p10k
    moved loading of p10k
    and cleaned the if structure in the file

--- user/shell/zshell.nix
+++ user/shell/zshell.nix
@@ -29,26 +29,27 @@ in
       MANROFFOPT = ( if ( config.programs.bat.enable == true) then "-c" else "");
       FZF_DEFAULT_OPTS="--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8,selected-bg:#45475a --multi";
     };
-    plugins = [] ++
-          ( if (userSettings.prompt == "p10k") then
-              [
-                {
-                  name = "powerlevel10k";
-                  src = pkgs.zsh-powerlevel10k;
-                  file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    plugins = lib.mkIf ( userSettings.prompt == "p10k")
+                 [
+                 {
+                   name = "powerlevel10k-config";
+                   src =  ./../apps/p10k;
+                   file = "p10k.zsh";
+                 }
+                  {
+                    name = "zsh-powerlevel10k";
+                    src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+                    file = "powerlevel10k.zsh-theme";
                 }
-              ]
-            else
-              []
-          );
-    initExtra = ( if (userSettings.prompt == "p10k") then
-      ''
-      source ~/nixos-config/dotfiles/p10k.zsh
-      ''
-    else
-      ''
-      ''
-      );
+              ];
+#    initExtra = ( if (userSettings.prompt == "p10k") then
+#      ''
+#      source ~/nixos-config/dotfiles/p10k.zsh
+#      ''
+#    else
+#      ''
+#      ''
+#      );
 		};
   programs.starship.enable = ( if (userSettings.prompt == "starship") then true else false);
   programs.starship.enableZshIntegration = ( if (userSettings.prompt == "starship") then true else false);
