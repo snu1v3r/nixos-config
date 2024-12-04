ae78c73d12a79c6d4618ebbad29c8cfaa3e06794
snu1v3r <snu1v3r@github.com>
2024-12-04 05:45:42 +0100

    optimize man and diff for bat
    optimize integration with bat batman and batdiff are used instead of
    tweaks in zsh. this is a more robust solution for diff and man using bat

--- home/user/common/bat/default.nix
+++ home/user/common/bat/default.nix
@@ -1,6 +1,19 @@
 { pkgs, ...}:
 
 {
-  programs.bat.enable = true;
+  programs.bat = {
+    enable = true;
+    config = {
+#      style = "numbers,changes,header";
+    };
+    extraPackages = builtins.attrValues {
+      inherit (pkgs.bat-extras)
+
+      batgrep
+      batdiff
+      batman
+      ;
+    };
+  };
 }
 
