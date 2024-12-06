3d996bede05ed5db06f16e3af1fa438104748bee
snu1v3r <snu1v3r@github.com>
2024-12-06 16:45:58 +0100

    adds editor as environment variable

--- home/user/common/zsh/default.nix
+++ home/user/common/zsh/default.nix
@@ -40,6 +40,7 @@ in
 #      );
 #      MANROFFOPT = (if (config.programs.bat.enable == true) then "-c" else "");
       FZF_DEFAULT_OPTS = "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8,selected-bg:#45475a --multi";
+      EDITOR = "vi";
     };
     plugins = lib.mkIf (userSettings.prompt == "p10k") [
       {
