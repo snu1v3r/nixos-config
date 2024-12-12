c1b5a9a238a5f59c61539ac82cb5fe8e45bc3b0e
snu1v3r <snu1v3r@github.com>
2024-12-12 20:04:43 +0100

    adds some options to neovim
    adds some option to load custom settings in neovim and to seperate

---
+++ home/user/common/nvim/config/lua/custom/plugins/options.lua
@@ -0,0 +1,7 @@
+-- Additional options for nvim
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
+
+return {
+}
--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -33,6 +33,7 @@
     }
   ];
 
+  nix.settings.allowed-users = [ "@wheel" "user" ];
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
