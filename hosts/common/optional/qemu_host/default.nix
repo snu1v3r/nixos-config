abbda30d966a9fec3fce651f6da3695a741fd1cc
snu1v3r <snu1v3r@github.com>
2024-12-31 09:51:33 +0100

    fixes virtualization host settings

--- hosts/common/optional/qemu_host/default.nix
+++ hosts/common/optional/qemu_host/default.nix
@@ -1,15 +1,17 @@
-{ pkgs,  ... }:
+{ pkgs, ... }:
 
 {
-	programs.virt-manager.enable = true;
-	# TODO Ensure that the username is taken from the settings
-	users.groups.libvirtd.members = ["user"];
-	virtualisation.libvirtd.enable = true;
-	virtualisation.spiceUSBRedirection.enable = true;
+  # TODO Ensure that the username is taken from the settings
+  users.groups.libvirtd.members = [ "user" ];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 
-	environment = {
-		systemPackages = [ pkgs.qemu ];
+  environment = {
+    systemPackages = [
+      pkgs.qemu
+      pkgs.virt-manager
+    ];
 
-	};
+  };
 
 }
