af815aa68a46203823c8c9f8b2a3367dd38ad7a2
snu1v3r <snu1v3r@github.com>
2025-01-01 10:34:12 +0100

    adds user file

---
+++ home/user/hawk/default.nix
@@ -0,0 +1,12 @@
+{ userSettings, ... }:
+{
+  imports = [
+    ../core
+    ../common/codium
+    ../common/flameshot
+    ../common/plasma
+    ../common/${userSettings.browser}
+    ../common/${userSettings.emulator} # This selects the terminal emulator
+  ];
+}
+
