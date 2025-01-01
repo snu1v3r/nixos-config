a93449adc30aabb9c998661ff5c77b794c5a6044
snu1v3r <snu1v3r@github.com>
2025-01-01 07:58:04 +0100

    small fixes to the install script

--- install_nixos.sh
+++ install_nixos.sh
@@ -39,12 +39,17 @@ if [ -f './flake.nix' ]; then
   echo "-----"
   echo "flake.nix exists install script will exit"
   echo "manually verify the content of flake.nix"
-  echo "and rebuild the system with"
-  echo "./rebuild.sh [system_name]"
+  echo "and rebuild the system with:"
+  echo ""
+  echo "  ./rebuild.sh [system_name]"
+  echo ""
+  echo "or remove the existing flake.nix and restart"
+  echo "the install script"
   exit
+else
+  cp ./flake_template.nix ./flake.nix
 fi
 
-cp ./flake_template.nix ./flake.nix
 
 read -rp "Which system to configure (walrus | snake | raptor | hawk | dragon | lizard ): [walrus] " system
 if [ -z "$system" ]; then
@@ -65,17 +70,19 @@ fi
 
 echo "-----"
 detect_systemd=`cat /etc/nixos/configuration.nix|sed -n "s/^.*systemd-boot.*\(true\|false\);$/\1/p"`
-if [ $detect_systemd = "true" ]; then
+if [ "$detect_systemd" == "true" ]; then
   echo "Systemd was detected as bootloader"
   sed -i "/^\s*bootLoader\s*=\s*/s/\"\(.*\)\"/\"systemd\"/" ./flake.nix
 else
   detect_grub=`cat /etc/nixos/configuration.nix|sed -n "s/^.*boot.loader.grub.enable.*\(true\|false\);$/\1/p"`
-  if [ $detect_grub = "true" ]; then
+  if [ "$detect_grub" == "true" ]; then
     echo "Grub was detected as bootloader"
     detect_device=`cat /etc/nixos/configuration.nix|sed -n "s/^.*boot.loader.grub.device.*\"\(.*\)\";$/\1/p"`
     echo "Grub boot device detected: $detect_device"
     sed -i "/^\s*bootLoader\s*=\s*/s/\"\(.*\)\"/\"grub\"/" ./flake.nix
-    sed -i "/^\s*bootDevice\s*=\s*/s/\"\(.*\)\"/\"$detect_device\"/" ./flake.nix
+
+    # : is used as an argument seperator in the sed command because of the / character in the device names
+    sed -i "/^\s*bootDevice\s*=\s*/s:\"\(.*\)\":\"$detect_device\":" ./flake.nix
   fi
 fi
 echo "-----"
@@ -85,5 +92,5 @@ sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/system/hardware-
 echo "-----"
 echo "Setting Required Nix Settings Then Going To Install"
 
-./rebuild.sh
+./rebuild.sh $system
 
